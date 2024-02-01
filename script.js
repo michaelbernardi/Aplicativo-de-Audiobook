9d4cb4ecf1d02285a3f1c3f639e627bb8bc5c29d
Michael Bernardi <michaelbernardi.silva@gmail.com>
2024-01-31 23:35:35 -0300

    Correção dos botões de voltar e avançar

--- script.js
+++ script.js
@@ -1,15 +1,65 @@
-const botaoPlayPause = document.getElementById('play-pause');
 const audioCapitulo = document.getElementById('audio-capitulo');
+const botaoPlayPause = document.getElementById('play-pause');
+const botaoAvancar = document.getElementById('proximo');
+const botaoVoltar = document.getElementById('anterior');
+const nomeCapitulo = document.getElementById('capitulo');
 
 const numeroCapitulo = 10;
 let taTocando = 0;
+let capituloAtual = 1;
 
 function tocarFaixa() {
     audioCapitulo.play();
+    botaoPlayPause.classList.remove('bi-play-circle-fill');
+    botaoPlayPause.classList.add('bi-pause-circle-fill');
 }
 function pausarFaixa(){
     audioCapitulo.pause();
+    botaoPlayPause.classList.remove('bi-pause-circle-fill');
+    botaoPlayPause.classList.add('bi-play-circle-fill');
 }
 
-botaoPlayPause.addEventListener('click', tocarFaixa);
+function tocarOuPausar(){
+    if(taTocando === 0){
+        tocarFaixa();
+        taTocando = 1;
+    }
+    else{
+        pausarFaixa();
+        taTocando = 0;
+    }
+}
 
+function trocarNomeFaixa(){
+    nomeCapitulo.innerText = 'Capitulo ' + capituloAtual;
+}
+
+function proximaFaixa(){
+    if(capituloAtual === numeroCapitulo) {
+        capituloAtual = 1;
+    } else{
+        capituloAtual = capituloAtual + 1;
+    }
+
+    audioCapitulo.src = "./books/dom-casmurro/" + capituloAtual + '.mp3';
+    tocarFaixa();
+    taTocando = 1;
+    trocarNomeFaixa()
+}
+
+function voltarFaixa(){
+    if(capituloAtual === 1) {
+        capituloAtual = numeroCapitulo;
+    } else{
+        capituloAtual = capituloAtual - 1;
+    }
+
+    audioCapitulo.src = "./books/dom-casmurro/" + capituloAtual + '.mp3';
+    tocarFaixa();
+    taTocando = 1;
+    trocarNomeFaixa()
+}
+
+botaoPlayPause.addEventListener('click', tocarOuPausar);
+botaoAvancar.addEventListener('click', proximaFaixa);
+botaoVoltar.addEventListener('click', voltarFaixa);
